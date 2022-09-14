ca9a2386229966e62fe57b9c8f99517cae6b4b53
Sávio Cunha <60552629+saviocunha2@users.noreply.github.com>
2022-09-14 14:27:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const  doc = fetch('./doc.txt');
+
+doc.then(resolucao =>{
+    return resolucao.text();
+}).then((body)=>{
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = body;
+    console.log(body);
+})
